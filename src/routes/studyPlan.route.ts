f594218398c56c0542d82929a172204203491fc7
DanielNwolu <nwoludaniel.c@gmail.com>
2025-05-14 11:09:04 +0100

    made some field optional in generate study plan and added study plan validator function

--- src/routes/studyPlan.route.ts
+++ src/routes/studyPlan.route.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import * as studyPlanController from "../controllers/studyPlan.controllers";
 import { authMiddleware } from "../middlewares/authentication.middlewares";
+import { generateStudyPlanValidator } from "../validators/studyPlan.validators";
+
 
 const router = express.Router();
 
@@ -9,7 +11,7 @@ const router = express.Router();
  * @desc Generate a study plan using AI
  * @access Private
  */
-router.post("/generate", authMiddleware, studyPlanController.generateStudyPlan);
+router.post("/generate", authMiddleware, generateStudyPlanValidator,studyPlanController.generateStudyPlan);
 router.get("/all", authMiddleware, studyPlanController.getAllStudyPlans);
 router.get("/:planId", authMiddleware, studyPlanController.getStudyPlan);
 router.delete("/:planId", authMiddleware, studyPlanController.deleteStudyPlan);
